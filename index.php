24b21b6484721147dcdc47fca0da4b08e1c2d99c
Ever Morgan <pierson12@hotmail.com>
2016-04-25 18:26:07 -0500

    prueba deployd 1

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.21/vue.min.js"></script>
+<script>
+	
+</script>
---
+++ prueba.php
@@ -0,0 +1,45 @@
+{ "user": [ { "id": "001", "name": "Raj Amal", "email": "raj.amalw@gmail.com" } ] }
+
+da200aa1b2ec3c15f860dbc71cfa2eb9965a15b2
+
+
+
+commit a62b596e84484afb0fa59802092e6a60588e5480
+Merge: da200aa dbf437c
+Author: Ever Morgan <ever.sanchez@myss.mx>
+Date:   Thu Apr 14 18:19:24 2016 -0500
+
+    archivos actualizados
+
+commit da200aa1b2ec3c15f860dbc71cfa2eb9965a15b2
+Author: Ever Morgan <ever.sanchez@myss.mx>
+Date:   Thu Apr 14 14:40:26 2016 -0500
+
+    ignored
+
+commit a1a1cac8fae58a1fbd7af028731f8e51cc93a21c
+Author: Ever Morgan <ever.sanchez@myss.mx>
+Date:   Thu Apr 14 13:35:26 2016 -0500
+
+    quitando imagenes qr
+
+commit d588013b7e1c9801804702b860ccb3644ca2b1a5
+Author: Ever Morgan <ever.sanchez@myss.mx>
+Date:   Thu Apr 14 13:27:41 2016 -0500
+
+    update 2
+
+commit 3544d07aabcd326425895e35c8c1b38eee406944
+Author: Ever Morgan <ever.sanchez@myss.mx>
+Date:   Thu Apr 14 13:14:26 2016 -0500
+
+    actualizacion paths news
+
+commit fee5bfdf8ba920c5259f324be27faba920d24e38
+Author: Ever Morgan <ever.sanchez@myss.mx>
+Date:   Thu Apr 14 12:39:49 2016 -0500
+
+    actualizacion de directorio depuracion de paths
+
+commit dbf437ccbe3875cf6d4d68cba44525159c011915
+Author: Ever Morgan <ever.sanchez@myss.mx>
